f30450219ac2c0cdb6a3eeda6637ce2f9b46f39c
sk2909 <128417368+sk2909@users.noreply.github.com>
2024-08-04 23:21:53 +0530

    Update 2.java

--- 2.java
+++ 2.java
@@ -1,6 +1,8 @@
 package com.example.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
                 String failureMsg = "Invalid Credentials";
                 if(str.equals(username) && sub.equals(password)){
                     Toast.makeText(getApplicationContext(),successMsg, Toast.LENGTH_LONG).show();
+                    Intent conn=new Intent(getApplicationContext(),MainActivity2.class);
+                    startActivity(conn);
                 }else {
                     Toast.makeText(getApplicationContext(),failureMsg, Toast.LENGTH_LONG).show();
                 }
